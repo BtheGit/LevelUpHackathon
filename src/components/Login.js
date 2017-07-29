4222ab564e9c202f622cb4c52a2ad5e241e36599
Horsetoast <peciar.matus@gmail.com>
2017-07-29 20:49:29 +0800

    Login UI update

--- src/components/Login.js
+++ src/components/Login.js
@@ -19,13 +19,14 @@ class Login extends Component {
 	render() {
 		return (
 			<div id="login-view">
-				<div id="customer-box">
-					<a id="customer-button"
-						onClick = {() => { this.toggle('customer') }}
-						className = {(this.state.customerForm ? 'hidden' : '')}>
+
+				<div id="customer-box" className="login-box">
+					<a id="customer-button" className={'login-circle-button ' + (this.state.customerForm ? 'hidden' : '')}
+						onClick = {() => { this.toggle('customer') }}>
 						<span>Customer</span>
 					</a>
-					<form id="login-form" className={'pure-form ' + (this.state.customerForm ? '' : 'hidden')}>
+
+					<form className={'pure-form login-form ' + (this.state.customerForm ? '' : 'hidden')}>
 						<fieldset className="pure-group">
 							<input type="email" placeholder="Email" />
 							<input type="password" placeholder="Password" />
@@ -33,15 +34,18 @@ class Login extends Component {
 						<button
 							className="pure-button pure-button-primary"
 							onClick={() => this.props.redirect('CUSTOMER')}>
-							CUSTOMER LOGIN
+							Log In
 						</button>
 					</form>
 				</div>
-				<div id="business-box">
-					<a id="business-button" onClick = {() => { this.toggle('business') }}>
+
+				<div id="business-box" className="login-box">
+					<a id="business-button" className="login-circle-button"
+						onClick = {() => { this.toggle('business') }}>
 						<span>Business</span>
 					</a>
 				</div>
+
 			</div>
 		)
 	}
