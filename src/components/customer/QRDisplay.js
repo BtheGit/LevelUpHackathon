ab89bcd9e8d46b79ac470f8d8228b3ee423e81bc
Horsetoast <peciar.matus@gmail.com>
2017-07-29 22:35:10 +0800

    Customer screen icons update + QR code

--- src/components/customer/QRDisplay.js
+++ src/components/customer/QRDisplay.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import QRCode from 'qrcode.react';
+import ReactSVG from 'react-svg';
 
 const QRDISPLAY = 'QRDISPLAY',
 			DISCOVER 	= 'DISCOVER',
 			MYSTORES 	= 'MYSTORES'
-
 
 class QRDisplay extends Component {
 	constructor(props){
@@ -16,13 +17,21 @@ class QRDisplay extends Component {
 		return (
 			<div id="customer-view" className="full-height-parent">
 				<div id="qr-container">
-					<div id="qr-code">QR CODE</div>
+					<div id="qr-code">
+						<QRCode value="http://facebook.github.io/react/" />
+					</div>
 				</div>
 				<a id="button-discover" className="button-circle" onClick={() => this.props.goto(DISCOVER)}>
 					<span>Discover</span>
+					<ReactSVG
+				    path="/images/icon_discover.svg"
+					/>
 				</a>
 				<a id="button-mystores" className="button-circle" onClick={() => this.props.goto(MYSTORES)}>
 					<span>My Stores</span>
+					<ReactSVG
+				    path="/images/icon_stamps.svg"
+					/>
 				</a>
 			</div>
 		)
