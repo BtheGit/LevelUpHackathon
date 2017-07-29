022635e2b4d5501a7330cefc96984fec4542c7ba
Horsetoast <peciar.matus@gmail.com>
2017-07-29 21:05:42 +0800

    Breadcrumb component

---
+++ src/components/partials/Breadcrumb.js
@@ -0,0 +1,11 @@
+import '../../css/breadcrumb.css';
+
+const Breadcrumb = (props) => {
+  return (
+    <div className="breadcrumb">
+      if (this.props.canReturn) {
+        <a className="button-back">Back</a>
+      }
+    </div>
+  )
+}
